4dd2b115674bde718a30caf92571a8e64a5253eb
Thomas Steen Petersen <tsp@neocortec.com>
2014-03-27 14:09:54 +0100

    first web app

---
+++ JS/index_user_scripts.js
@@ -0,0 +1,46 @@
+(function()
+{
+ "use strict";
+ /*
+   hook up event handlers 
+ */
+ function register_event_handlers()
+ {
+    
+    
+         $(".uib_w_4").click(function(evt)
+        {
+         /* Other possible functions are: 
+           uib_sb.open_sidebar($sb)
+           uib_sb.close_sidebar($sb)
+           uib_sb.toggle_sidebar($sb)
+            uib_sb.close_all_sidebars()
+          See js/sidebar.js for the full sidebar API */
+        
+         uib_sb.toggle_sidebar($(".uib_w_2"));  
+        });
+        $(".uib_w_5").click(function(evt)
+        {
+         /* Other possible functions are: 
+           uib_sb.open_sidebar($sb)
+           uib_sb.close_sidebar($sb)
+           uib_sb.toggle_sidebar($sb)
+            uib_sb.close_all_sidebars()
+          See js/sidebar.js for the full sidebar API */
+        
+         uib_sb.toggle_sidebar($(".uib_w_2"));  
+        });
+        $(".uib_w_11").click(function(evt)
+        {
+         /* Other possible functions are: 
+           uib_sb.open_sidebar($sb)
+           uib_sb.close_sidebar($sb)
+           uib_sb.toggle_sidebar($sb)
+            uib_sb.close_all_sidebars()
+          See js/sidebar.js for the full sidebar API */
+        
+         uib_sb.toggle_sidebar($(".uib_w_2"));  
+        });
+}
+ $(document).ready(register_event_handlers);
+})();
