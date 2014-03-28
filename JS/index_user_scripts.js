ed7f86774a0ac6e7b7c17ca598a480864d276407
Thomas Steen Petersen <tsp@neocortec.com>
2014-03-28 11:52:29 +0100

    1

--- JS/index_user_scripts.js
+++ JS/index_user_scripts.js
@@ -8,7 +8,31 @@
  {
     
     
-         $(".uib_w_3").click(function(evt)
+         $(".uib_w_4").click(function(evt)
+        {
+         /* Other possible functions are: 
+           uib_sb.open_sidebar($sb)
+           uib_sb.close_sidebar($sb)
+           uib_sb.toggle_sidebar($sb)
+            uib_sb.close_all_sidebars()
+          See js/sidebar.js for the full sidebar API */
+        
+         uib_sb.toggle_sidebar($(".uib_w_2"));  
+        });
+        
+        
+        $("#btn_settings").click(function(evt)
+        {
+         /* Other possible functions are: 
+           uib_sb.open_sidebar($sb)
+           uib_sb.close_sidebar($sb)
+           uib_sb.toggle_sidebar($sb)
+            uib_sb.close_all_sidebars()
+          See js/sidebar.js for the full sidebar API */
+        
+         uib_sb.toggle_sidebar($(".uib_w_2"));  
+        });
+        $("#btn_sidebar_ok").click(function(evt)
         {
          /* Other possible functions are: 
            uib_sb.open_sidebar($sb)
